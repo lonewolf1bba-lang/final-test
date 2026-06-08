7ddda8417fd8bc02ec1cabe5247f67f198299dc4
lonewolf1bba-lang <lonewolf1bba@gmail.com>
2026-06-08 12:30:11 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path"
+import { fileURLToPath } from "url"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
+})
